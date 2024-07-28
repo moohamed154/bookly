a10b46f281deee32538c84cf50ee2ad51a39a078
moohamed154 <medohs770@gmail.com>
2024-07-28 19:18:03 +0300

    add featured list view to home

--- lib/Features/home/data/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/home/data/presentation/views/widgets/custom_app_bar.dart
@@ -8,9 +8,11 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 24,
-        vertical: 50,
+      padding: const EdgeInsets.only(
+        left: 24,
+        right: 24,
+        top: 50,
+        bottom: 20,
       ),
       child: Row(
         children: [
--- lib/Features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,8 +11,29 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListVIew(),
       ],
+    );
+  }
+}
+
+class FeaturedBooksListVIew extends StatelessWidget {
+  const FeaturedBooksListVIew({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 8,
+              ),
+              child: FeaturedListViewItem(),
+            );
+          }),
     );
   }
 }
