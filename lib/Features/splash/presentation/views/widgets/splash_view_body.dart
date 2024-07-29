4ae9f38e6037f1b5499c6a1e7b41579654fadd85
moohamed154 <medohs770@gmail.com>
2024-07-29 20:27:32 +0300

    refactor navigation in our app

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,8 @@
-import 'package:bookly_app/Features/home/data/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
-import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -61,9 +60,13 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   //navigate to home screen
   void navigateToHome() {
-    Future.delayed(const Duration(seconds: 2), () {
-      Get.off(() => const HomeView(),
-          transition: Transition.fade, duration: kTranstionDurration);
-    });
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        GoRouter.of(context).go(
+          AppRouter.kHomeView,
+        );
+      },
+    );
   }
 }
