24fff3d1227e69f6e15dcbbbb821aa0a0b2d3c01
moohamed154 <medohs770@gmail.com>
2024-07-29 20:39:21 +0300

    create Custom Book Details AppBar

--- lib/Features/home/data/presentation/views/widgets/book_dtails_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/book_dtails_view_body.dart
@@ -1,10 +1,20 @@
-import 'package:flutter/cupertino.dart';
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 30,
+      ),
+      child: Column(
+        children: [
+          CustomBookDetailsAppBar(),
+        ],
+      ),
+    );
   }
 }
