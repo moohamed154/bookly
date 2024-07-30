6ee8a7df6437559571c4635436d810c2af0368a2
moohamed154 <medohs770@gmail.com>
2024-07-30 19:23:09 +0300

    create books action

--- lib/Features/home/data/presentation/views/widgets/book_dtails_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/book_dtails_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -51,6 +53,10 @@ class BookDetailsViewBody extends StatelessWidget {
           const BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
           ),
+          const SizedBox(
+            height: 37,
+          ),
+          const BooksAction(),
         ],
       ),
     );
