ee3e927d961cc272b22871bf28bbbad92e990129
moohamed154 <medohs770@gmail.com>
2024-08-02 15:01:17 +0300

    create server failure rom dioexception

--- lib/Features/home/data/rebos/home_repo_implementation.dart
+++ lib/Features/home/data/rebos/home_repo_implementation.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/data/rebos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImplementation implements HomeRepo {
   final ApiService apiService;
@@ -21,7 +22,10 @@ class HomeRepoImplementation implements HomeRepo {
 
       return right(books);
     } on Exception catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
