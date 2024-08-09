c3a8071d557c258a34396cc35301b5d0368b3128
moohamed154 <medohs770@gmail.com>
2024-08-09 15:57:50 +0300

    commit

--- lib/Features/home/presentation/views/widgets/books_action.dart
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -2,7 +2,6 @@ import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart'
 import 'package:bookly_app/core/utils/functions/launch_url.dart';
 import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({super.key, required this.bookModel});
